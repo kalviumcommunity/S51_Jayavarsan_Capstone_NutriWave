a37f4f6856cc92582bf72a829cbfd30cc96f21b1
jayavarsan <jayavarsanr@gmail.com>
2024-03-20 10:23:07 +0530

    connected database with my server

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,21 @@
-const express = require('express')
-const app =  express()
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.get('/' ,(req,res)=>{
-    res.send("This is nutriWave wait for updates")
-})
+const app = express();
 
-app.listen(3000 , ()=>{
-    console.log("Nutriwave server started running  on port 3000");
-})
+
+const uri = "mongodb+srv://geekygure:Ugen0bdtJ2rJJ32p@nutriwave.bwjcxkj.mongodb.net/?retryWrites=true&w=majority&appName=nutriwave";
+
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(err => console.error("Error connecting to MongoDB:", err));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send("This is nutriWave. Wait for updates.");
+});
+
+app.listen(3000, () => {
+  console.log("Nutriwave server started running on port 3000");
+});
