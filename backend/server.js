01cacdf0ccdf755071be8d450d70201f2094f272
jayavarsan <jayavarsanr@gmail.com>
2024-04-08 19:15:50 +0530

    seeting up and connec ting database for signin

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
+require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose');
-
+// const cros =require('cros');
 const app = express();
 
+app.use(express.json())
+// app.use(cros())
 
-const uri = "mongodb+srv://geekygure:Ugen0bdtJ2rJJ32p@nutriwave.bwjcxkj.mongodb.net/?retryWrites=true&w=majority&appName=nutriwave";
+
+
+const uri = process.env.DDURL;
 
 mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Connected to MongoDB"))
@@ -16,6 +21,6 @@ app.get('/', (req, res) => {
   res.send("This is nutriWave. Wait for updates.");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Nutriwave server started running on port 3000");
 });
